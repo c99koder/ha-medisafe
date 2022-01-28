dac7c029751687894aad5a3010ada2c1ffbc8ef1
Sam Steele <sam.steele@gmail.com>
2022-01-28 08:57:15 -0500

    Improve description text

--- custom_components/medisafe/api.py
+++ custom_components/medisafe/api.py
@@ -56,7 +56,7 @@ class MedisafeApiClient:
         self, method: str, url: str, data: dict = {}, headers: dict = {}
     ) -> dict:
         try:
-            async with async_timeout.timeout(TIMEOUT, loop=asyncio.get_event_loop()):
+            async with async_timeout.timeout(TIMEOUT):
                 if method == "get":
                     response = await self._session.get(url, headers=headers)
                     return await response.json()
